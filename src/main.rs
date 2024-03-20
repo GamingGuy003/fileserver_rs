cd18a05393e85b50bf83ee2baa3dd8ba90715170
GamingGuy003 <bjornfelixh@gmail.com>
2024-03-20 18:06:36 +0100

    Changed log calls + clippy happy

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::{
 
 use cali::parser::Parser;
 use http_serv::{http_server::server::HttpServer, HttpData, HttpRequest, HttpResponse, HttpStatus};
-use log::{debug, info, warn};
 
 fn main() {
     pretty_env_logger::formatted_timed_builder()
@@ -39,30 +38,30 @@ fn main() {
     parser.parse().expect("Failed to parse arguments");
 
     let addr = if let Some(pa) = parser.get_parsed_argument_long("addr") {
-        info!("Trying to use supplied value for addr...");
+        log::info!("Trying to use supplied value for addr...");
         pa.value.unwrap_or("127.0.0.1".to_owned())
     } else {
-        info!("Using default value 127.0.0.1 for addr...");
+        log::info!("Using default value 127.0.0.1 for addr...");
         "127.0.0.1".to_owned()
     };
 
     let port = if let Some(pa) = parser.get_parsed_argument_long("port") {
-        info!("Trying to use supplied value for port...");
+        log::info!("Trying to use supplied value for port...");
         pa.value.unwrap_or("8080".to_owned())
     } else {
-        info!("Using default value 8080 for port...");
+        log::info!("Using default value 8080 for port...");
         "8080".to_owned()
     };
 
     let root = if let Some(pa) = parser.get_parsed_argument_long("root") {
-        info!("Trying to use supplied value for root...");
+        log::info!("Trying to use supplied value for root...");
         pa.value.unwrap_or(".".to_owned())
     } else {
-        info!("Using default root folder where instance has been launched...");
+        log::info!("Using default root folder where instance has been launched...");
         ".".to_owned()
     };
 
-    info!("Starting server on {addr}:{port}, serving '{}'...", root);
+    log::info!("Starting server on {addr}:{port}, serving '{}'...", root);
 
     HttpServer::new(
         addr,
@@ -70,7 +69,7 @@ fn main() {
         4,
         Vec::new(),
         Some(Box::new(|request: &HttpRequest| {
-            debug!("Got unimplemented request {:?}", request);
+            log::debug!("Got unimplemented request {:?}", request);
             HttpResponse {
                 data: Some(HttpData::Bytes(
                     format!("Not implemented. Request was:<br>{:#?}", request)
@@ -88,7 +87,7 @@ fn main() {
             let uri = match request.get_route_param(":uri*".to_owned()) {
                 Some(uri) => uri,
                 None => {
-                    warn!("URI not set");
+                    log::warn!("URI not set");
                     return HttpResponse::new("1.1".to_owned(), HttpStatus::NotFound, None, None);
                 }
             };
@@ -96,7 +95,7 @@ fn main() {
             let uri_path = Path::new(&uri);
             let rooted_path = root_path.join(uri_path);
 
-            debug!("Trying to handle path {}", uri_path.display());
+            log::debug!("Trying to handle path {}", uri_path.display());
             match rooted_path {
                 _ if rooted_path.is_dir() => {
                     handle_folder(uri_path.to_path_buf(), root_path.to_path_buf())
@@ -105,7 +104,7 @@ fn main() {
                     handle_file(uri_path.to_path_buf(), root_path.to_path_buf())
                 }
                 _ => {
-                    warn!("File {} not found", rooted_path.display());
+                    log::warn!("File {} not found", rooted_path.display());
                     HttpResponse::new(
                         "1.1".to_owned(),
                         HttpStatus::NotFound,
@@ -129,19 +128,19 @@ pub fn handle_file(path: PathBuf, root: PathBuf) -> HttpResponse {
     let file = match File::open(&path) {
         Ok(file) => file,
         Err(err) => {
-            warn!("Could not open file: {err}");
+            log::warn!("Could not open file: {err}");
             return HttpResponse::new("1.1".to_owned(), HttpStatus::NotFound, None, None);
         }
     };
     let len = match file.metadata() {
         Ok(metadata) => metadata.len() as usize,
         Err(err) => {
-            warn!("Could not get metadat: {err}");
+            log::warn!("Could not get metadat: {err}");
             return HttpResponse::new("1.1".to_owned(), HttpStatus::NotFound, None, None);
         }
     };
 
-    debug!("Sending file {} with length {}", path.display(), len);
+    log::debug!("Sending file {} with length {}", path.display(), len);
     HttpResponse {
         data: Some(HttpData::Stream((Box::new(file), len))),
         ..Default::default()
@@ -150,10 +149,10 @@ pub fn handle_file(path: PathBuf, root: PathBuf) -> HttpResponse {
 
 pub fn handle_folder(path: PathBuf, root: PathBuf) -> HttpResponse {
     let rooted_path = root.join(&path);
-    let files = match fs::read_dir(&rooted_path) {
+    let files = match fs::read_dir(rooted_path) {
         Ok(file) => file,
         Err(err) => {
-            warn!("Could not open file: {err}");
+            log::warn!("Could not open file: {err}");
             return HttpResponse::new("1.1".to_owned(), HttpStatus::NotFound, None, None);
         }
     };
@@ -170,8 +169,7 @@ pub fn handle_folder(path: PathBuf, root: PathBuf) -> HttpResponse {
         &mut files
             .map_while(Result::ok)
             .map(|entry| {
-                let binding = entry
-                    .path();
+                let binding = entry.path();
                 let entry_string = binding
                     .strip_prefix(&root)
                     .unwrap_or(Path::new("."))
@@ -186,7 +184,7 @@ pub fn handle_folder(path: PathBuf, root: PathBuf) -> HttpResponse {
     );
     entries.sort();
 
-    debug!("Sending {} folder entries...", entries.len());
+    log::debug!("Sending {} folder entries...", entries.len());
     HttpResponse {
         data: Some(HttpData::Bytes(entries.join("<br>").as_bytes().to_vec())),
         ..Default::default()
